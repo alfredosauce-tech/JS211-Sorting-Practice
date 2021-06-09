e79e25547794e681097b49715ef97e3553ff5cd9
mayrant <mayrant2004@yahoo.com>
2021-06-09 03:03:11 -0500

    weather functions working

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ How to Submit:
 
 - [x] Create a new repo called: "JS-Sorting-Practice".
 - [x] Get into the practice of working with branches by creating a new branch called "sorting-branch".
-- [ ] Complete a code plan.
+- [x] Complete a code plan.
 - [x] Create a new file called `sorting.js`.
-- [ ] Copy/paste & complete the code below using the rubric below.
-- [ ] Make a pull request against your master branch and submit the URL to it as your assignment.
+- [x] Copy/paste & complete the code below using the rubric below.
+- [x] Make a pull request against your master branch and submit the URL to it as your assignment.
 
 Rubric:
 
@@ -19,18 +19,10 @@ Rubric:
 Strings to Nums:
 
 * Given 1000 digits of PI as strings, return an array of the digits as numbers
-- function stringsToNums that takes in an arr
-- loop through the arr to .parseInt() each string to a number
-- return those numbers in a new array, numsArr
 
 * With the same numbers, find the sum of the even values
-- find the even numbers of the numsArr
-- if number % 2 === 0, return that number in a new arr, evens
-- reduce evens to find the sum, (acc, curr) => acc + curr;
-- evens.reduce(sum)
 
 * Find the index of the first value that = 512 when added to it's index (#ATX!!)
-- findIndex of value when value + index === 512
 
 Weather:
 
--- sorting.js
+++ sorting.js
@@ -3,6 +3,9 @@ const strNums = ["1","4","1","5","9","2","6","5","3","5","8","9","7","9","3","2"
 // STRINGS TO NUMS /////
 
 // Given 1000 digits of PI as strings, return an array of the digits as numbers
+// function stringsToNums that takes in an arr
+// loop through the arr to .parseInt() each string to a number
+// return those numbers in a new array
 const stringsToNums = (arr) => {
   let newArr = [];
   arr.forEach((number) => {
@@ -16,6 +19,9 @@ console.log('Array of Numbers:', numsArr);
 
 
 // With the same numbers, find the sum of the even values
+// find the even numbers of the numsArr
+// if number % 2 === 0, return that number in a new arr, evens
+// reduce evens to find the sum, then return the sum
 const sumEvens = (arr) => {
   let evens = [];
   arr.forEach((number) => {
@@ -29,87 +35,112 @@ const sumEvens = (arr) => {
 
 console.log('Sum of Even Numbers:', sumEvens(numsArr));
 
-// Find the index of the first value when added to it's index = 512 (#ATX!!)
+// Find the index of the first value that = 512 when added to it's index (#ATX!!)
+// findIndex of value when value + index === 512
 const atxIdx = numsArr.findIndex((num, index) => {
   return num + index === 512;
 })
 
-console.log(`index: ${atxIdx}, value: ${numsArr[atxIdx]}`);
+console.log(`The ATX Index totals 512: index: ${atxIdx}, value: ${numsArr[atxIdx]}`);
 
 // WEATHER SORT /////
 
-// const weather = [
-//   { id: 5743823523151872,
-//     weather_state_name: "Light Cloud",
-//     weather_state_abbr: "lc",
-//     wind_direction_compass: "NNE",
-//     created: "2018-07-11T20:53:03.251710Z",
-//     applicable_date: "2018-07-11",
-//     min_temp: 14.43,
-//     max_temp: 23.36,
-//     the_temp: 22.785,
-//     wind_speed: 5.682503989556987,
-//     wind_direction: 21.6264939172659,
-//     air_pressure: 1024.45,
-//     humidity: 58,
-//     visibility: 8.683041040324504,
-//     predictability: 70
-//   },
-//   { id: 6188149969518592,
-//     weather_state_name: "Heavy Cloud",
-//     weather_state_abbr: "hc",
-//     wind_direction_compass: "NE",
-//     created: "2018-07-11T20:53:03.268190Z",
-//     applicable_date: "2018-07-12",
-//     min_temp: 14.81,
-//     max_temp: 25.52,
-//     the_temp: 24.61,
-//     wind_speed: 3.2461141472739206,
-//     wind_direction: 42.72552812997726,
-//     air_pressure: 1024.605,
-//     humidity: 54,
-//     visibility: 10.633835898353615,
-//     predictability: 71
-//   },
-//   { id: 5742049676492800,
-//     weather_state_name: "Showers",
-//     weather_state_abbr: "s",
-//     wind_direction_compass: "E",
-//     created: "2018-07-11T20:53:03.947390Z",
-//     applicable_date: "2018-07-13",
-//     min_temp: 15.5525,
-//     max_temp: 25.3475,
-//     the_temp: 24.175,
-//     wind_speed: 3.6572546846814604,
-//     wind_direction: 90.32910675612557,
-//     air_pressure: 1025.385,
-//     humidity: 57,
-//     visibility: 10.181166984808717,
-//     predictability: 73  
-//   },
-//   { id: 6696130918219776,
-//     weather_state_name: "Heavy Cloud",
-//     weather_state_abbr: "hc",
-//     wind_direction_compass: "SSW",
-//     created: "2018-07-11T20:53:04.068570Z",
-//     applicable_date: "2018-07-14",
-//     min_temp: 15.915,
-//     max_temp: 27.0925,
-//     the_temp: 26.585,
-//     wind_speed: 3.649847972759087,
-//     wind_direction: 200.04283406736377,
-//     air_pressure: 1024.4450000000002,
-//     humidity: 52,
-//     visibility: 11.14056410562316,
-//     predictability: 71
-//   },
-// ],
+const weather = [
+  { id: 5743823523151872,
+    weather_state_name: "Light Cloud",
+    weather_state_abbr: "lc",
+    wind_direction_compass: "NNE",
+    created: "2018-07-11T20:53:03.251710Z",
+    applicable_date: "2018-07-11",
+    min_temp: 14.43,
+    max_temp: 23.36,
+    the_temp: 22.785,
+    wind_speed: 5.682503989556987,
+    wind_direction: 21.6264939172659,
+    air_pressure: 1024.45,
+    humidity: 58,
+    visibility: 8.683041040324504,
+    predictability: 70
+  },
+  { id: 6188149969518592,
+    weather_state_name: "Heavy Cloud",
+    weather_state_abbr: "hc",
+    wind_direction_compass: "NE",
+    created: "2018-07-11T20:53:03.268190Z",
+    applicable_date: "2018-07-12",
+    min_temp: 14.81,
+    max_temp: 25.52,
+    the_temp: 24.61,
+    wind_speed: 3.2461141472739206,
+    wind_direction: 42.72552812997726,
+    air_pressure: 1024.605,
+    humidity: 54,
+    visibility: 10.633835898353615,
+    predictability: 71
+  },
+  { id: 5742049676492800,
+    weather_state_name: "Showers",
+    weather_state_abbr: "s",
+    wind_direction_compass: "E",
+    created: "2018-07-11T20:53:03.947390Z",
+    applicable_date: "2018-07-13",
+    min_temp: 15.5525,
+    max_temp: 25.3475,
+    the_temp: 24.175,
+    wind_speed: 3.6572546846814604,
+    wind_direction: 90.32910675612557,
+    air_pressure: 1025.385,
+    humidity: 57,
+    visibility: 10.181166984808717,
+    predictability: 73  
+  },
+  { id: 6696130918219776,
+    weather_state_name: "Heavy Cloud",
+    weather_state_abbr: "hc",
+    wind_direction_compass: "SSW",
+    created: "2018-07-11T20:53:04.068570Z",
+    applicable_date: "2018-07-14",
+    min_temp: 15.915,
+    max_temp: 27.0925,
+    the_temp: 26.585,
+    wind_speed: 3.649847972759087,
+    wind_direction: 200.04283406736377,
+    air_pressure: 1024.4450000000002,
+    humidity: 52,
+    visibility: 11.14056410562316,
+    predictability: 71
+  },
+];
 
 // using a higher order function, create an array of the unique 'weather_state_name' values of the weather array. Your function should return the following array ['Light Cloud', 'Heavy Cloud', 'Showers']
-// const weatherStates = 
-// console.log(weatherStates)
+// weather array of objects - weather[].weather_state_name
+// forEach object, return the weather_state_name in a new array
+// loop through the new array to find the unique weather_state_name values
+// if i and the indexOf i match, return the unique result
+const weatherStates = (arr) => {
+  let weatherArr = [];
+  let resultArr = [];
+  arr.forEach((obj) => {
+    weatherArr.push(obj.weather_state_name);
+  });
+  for (let i = 0; i < weatherArr.length; i++) {
+    if (i === weatherArr.indexOf(weatherArr[i])) {
+      resultArr.push(weatherArr[i]);
+    }
+  }
+  return resultArr;
+}
+console.log('Unique Weather State Names:', weatherStates(weather));
 
 //find the id of the object in weather that has a min_temp of 15.915
+// loop through weather array
+// if weather[i].min_temp === 15.915, return weather[i].id
+const idealTemp = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (weather[i].min_temp === 15.915) {
+      return weather[i].id;
+    }
+  }
+}
 
-// const idealTemp =
-// console.log(idealTemp)
+console.log('Ideal Temp ID:', idealTemp(weather));
